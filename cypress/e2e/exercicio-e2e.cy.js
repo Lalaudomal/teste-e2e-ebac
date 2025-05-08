8eceffa5f50fe9352c1e7d9b7f63a56940fdb3cb
Lalaudomal <lucasdark30@gmail.com>
2025-05-08 02:09:54 -0300

    first commit

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -14,9 +14,42 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.get('.post-2559 > .product-block').click()
+    cy.get('.button-variable-item-M').click()
+    cy.get('.button-variable-item-Green').click()
+    cy.get('.plus').click().click()
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.get('.post-2622 > .product-block > .caption > .meta > .infor > .name > a').click()
+    cy.get('.button-variable-item-L').click()
+    cy.get('.button-variable-item-Red').click()
+    cy.get('.input-text').clear().type('2')
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.get(':nth-child(3) > .page-numbers').click()
+    cy.get('.post-3098 > .product-block > .caption > .meta > .infor > .name > a').click()
+    cy.get('.button-variable-item-33').click()
+    cy.get('.button-variable-item-Blue').click()
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.get('.post-3528 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+    cy.get('.button-variable-item-32').click()
+    cy.get('.button-variable-item-Blue').click()
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.woocommerce-message > .button').click()
+    cy.get('.checkout-button').click()
+    cy.get('#billing_first_name').type('Lucas')
+    cy.get('#billing_last_name').type('Pereira')
+    cy.get('#billing_address_1').type('Rua tupi, 202')
+    cy.get('#billing_city').type('São Paulo')
+    cy.get('#billing_postcode').type('11111-111')
+    cy.get('#billing_phone').type('2222-222')
+    cy.get('#billing_email').type('aluno_ebac@teste.com')
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+    cy.get('.woocommerce-order-overview').should('exist')
+});
 
 
 })
